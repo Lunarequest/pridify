33397112c316baf54597c6fd6403d7ea2e7535e0
Luna <luna.dragon@suse.com>
2023-05-20 14:36:00 +0530

    pridfy wip

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+mod utils;
+
+use std::convert::TryInto;
+
+use image::{
+    imageops::overlay, load_from_memory, DynamicImage, GenericImage, GenericImageView, ImageBuffer,
+};
+use js_sys::{ArrayBuffer, Uint8Array};
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub fn girls(pfp_bytes: &[u8], flag_bytes: &[u8]) -> Result<Uint8Array, JsError> {
+    #[cfg(debug_assertions)]
+    utils::set_panic_hook();
+
+    let pfp = load_from_memory(pfp_bytes)?;
+    let flag = load_from_memory(flag_bytes)?;
+    let (width_pfp, hieght_pfp) = pfp.dimensions();
+    let (width_flag, hieght_flag) = flag.dimensions();
+
+    let x = (width_flag - width_pfp) / 2;
+    let y = (hieght_flag - hieght_pfp) / 2;
+
+    let buf = ImageBuffer::new(hieght_flag, hieght_flag);
+
+    let mut result = DynamicImage::ImageRgb8(buf);
+    result.copy_from(&flag, 0, 0)?;
+
+    overlay(&mut result, &pfp, i64::from(x), i64::from(y));
+
+    let bytes: Vec<u8> = result.into_bytes();
+    let inter = u32::from_be_bytes((&bytes[..4]).try_into().unwrap());
+    let buf = ArrayBuffer::new(inter);
+    let array = Uint8Array::new(&buf);
+
+    Ok(array)
+}
